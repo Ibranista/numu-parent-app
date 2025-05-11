397fa6f0efa96e58f8c5ba36f6006ee2c7aaa23f
Ibrahim Kedir <techofreact@gmail.com>
2025-05-11 21:59:23 +0300

    additional types for therapists and child features.

--- features/child/types.ts
+++ features/child/types.ts
@@ -4,15 +4,9 @@ export interface IConcern {
     description: string;
 }
 
-export interface IChildResponse {
-    id: string;
-    name: string;
-    gender: "male" | "female";
-    birthDate: string;
-    parent: string;
-    concerns: IConcern[];
-    therapist_matches: string[];
-    acceptedTherapists: string[];
+export interface IChildrenResponse extends IGetChildrenArgs {
+    total: number;
+    results: IChild[];
 }
 
 export interface IChild {
@@ -21,4 +15,36 @@ export interface IChild {
     gender: "male" | "female" | "";
     birthDate: string;
     concern_ids: string[];
+    concerns: IConcern[];
+    therapist_matches: ITherapistMatch[];
+    acceptedTherapists: any[];
 };
+
+export interface ITherapist {
+    id: number;
+    name: string;
+    image: string;
+    expertise: IExpertise[];
+    experience_years: number;
+    bio: string;
+    createdDate: string;
+}
+
+export interface ITherapistMatch {
+    id: number;
+    therapist: ITherapist;
+    status: string;
+    decline_reason: string;
+    created_at: string;
+    updated_at: string;
+}
+
+export interface IExpertise {
+    id: number;
+    expertise: string;
+}
+
+export interface IGetChildrenArgs {
+    page?: number;
+    limit?: number;
+}
