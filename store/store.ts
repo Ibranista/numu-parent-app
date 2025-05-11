b3bbb791fb6a25c1a2bbdb19cb1c61fee5cdf15c
Ibrahim Kedir <techofreact@gmail.com>
2025-05-11 12:13:00 +0300

    fix: prevent state from reseting and asking for logging in again when page refresh or app restarts.

--- store/store.ts
+++ store/store.ts
@@ -1,13 +1,34 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers } from "redux";
+import { FLUSH, PAUSE, PERSIST, persistReducer, persistStore, PURGE, REGISTER, REHYDRATE } from "redux-persist";
 import authReducer from "../features/auth/authSlice";
 import counterReducer from "../features/counter/counter.slice";
 
-export const store = configureStore({
-    reducer: {
-        counter: counterReducer,
-        auth: authReducer,
-    },
+const rootReducer = combineReducers({
+    counter: counterReducer,
+    auth: authReducer,
 });
 
+const persistConfig = {
+    key: "root",
+    storage: AsyncStorage,
+    // helps to persist my auth slice
+    whitelist: ["auth"],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+            },
+        }),
+});
+
+export const persistor = persistStore(store);
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
